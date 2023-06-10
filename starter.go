8c272ce229d12290dd37ff84f9130830c122cc24
Lawrance <nonham00@gmail.com>
2023-06-10 14:47:15 +0700

    New

--- starter.go
+++ starter.go
@@ -7,3 +7,10 @@ import (
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello %v. Welcome!", name)
 }
+func OddOrEven(number int) string {
+	oddOrEven := "odd"
+	if number%2 == 0 {
+		oddOrEven = "even"
+	}
+	return fmt.Sprintf("%v is an %v number", number, oddOrEven)
+}
--- starter_test.go
+++ starter_test.go
@@ -13,3 +13,28 @@ func TestSayHello(t *testing.T) {
 	another_greeting := SayHello("Fern")
 	assert.Equal(t, "Hello Fern. Welcome!", another_greeting)
 }
+
+func TestOddOrEven(t *testing.T) {
+	t.Run("Check Non Negative Even Numbers", func(t *testing.T) {
+		assert.Equal(t, "50 is an even number", OddOrEven(50))
+		assert.Equal(t, "42 is an even number", OddOrEven(42))
+		assert.Equal(t, "0 is an even number", OddOrEven(0))
+	})
+	t.Run("Check Negative Even Numbers", func(t *testing.T) {
+		assert.Equal(t, "-50 is an even number", OddOrEven(-50))
+		assert.Equal(t, "-42 is an even number", OddOrEven(-42))
+		assert.Equal(t, "-100 is an even number", OddOrEven(-100))
+	})
+	t.Run("Check Non Negative Odd Numbers", func(t *testing.T) {
+		assert.Equal(t, "3 is an odd number", OddOrEven((3)))
+		assert.Equal(t, "55 is an odd number", OddOrEven(55))
+		assert.Equal(t, "101 is an odd number", OddOrEven(101))
+	})
+	t.Run("Check Negative Odd Numbers", func(t *testing.T) {
+		assert.Equal(t, "-45 is an odd number", OddOrEven(-45))
+		assert.Equal(t, "-1 is an odd number", OddOrEven(-1))
+		assert.Equal(t, "-101 is an odd number", OddOrEven(-101))
+	})
+} 
+
+
